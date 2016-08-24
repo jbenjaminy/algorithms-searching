4c63cf4e7741ef7e9f2b49548cef1d691764d5d5
jbenjaminy <young.bejamin@gmail.com>
2016-08-24 11:25:35 -0500

    adds interview questions

---
+++ interview-questions.js
@@ -0,0 +1,6 @@
+
+   /* The share price for a company over a week's trading is as follows: [128, 97, 121, 123, 98, 97, 105]. If you had to buy shares in the company on one day, and sell the shares on one of the following days, write an algorithm to work out what the maximum profit you could make would be. */
+
+   /* Imagine that you wanted to find what the highest floor of a 100 story building you could drop an egg was, without the egg breaking. But you only have two eggs. Write an algorithm to work out which floors you should drop the eggs from to find this out in the most efficient way. */
+    
+   /* Imagine you are looking for a book in a library with a Dewey Decimal index. How would you go about it? Can you express this process as a searching algorithm? */
